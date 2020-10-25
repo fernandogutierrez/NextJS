89b22dd959829bd6b54ac53becae8c8001c6548e
fernandogutierrez279 <fernandogutierrez279@gmail.com>
2020-10-25 18:29:34 -0400

    Included setState to store the data inserted by the user

--- src/containers/Register/index.js
+++ src/containers/Register/index.js
@@ -11,39 +11,38 @@ const tailLayout = {
   },
 };
 
-const Register = ( {name = "/register"} ) =>{   
+const Register = () =>{   
   const router = useRouter();
-  const [pageState, setPageState] = useState({ name });
+  const [state, setState] = useState( {} );
 
   useEffect(() => {
     console.log("useEffect")
-    initialState();
       return () =>    {
           console.log(`Page: ${router.route} was changed!`);
         }
   }, []);
-
-  const initialState = async () =>{
-      setPageState({ name: router.route });
-  }
-  console.log(`Page loaded: ${router.route}!`);
-  console.log(pageState);
 
   const goBack = () => {
       router.push("/login");
   }
 
   const onFinish = (values) => {
-    console.log('Success:', values.firstName);
+    setState({ 
+        name: values.firstName,
+        lastName: values.LastName,
+        age: values.Age,
+        email: values.EmailAddress,
+        password: values.EmailAddress
+    });
 
     notification.open({
       message: 'Data inserted in FORM:',
       description:
-        `FirstName: ${values.firstName}
-         LastName: ${values.LastName}
-         Age: ${values.Age}
-         EmailAddress: ${values.EmailAddress}
-         Password: ${values.Password}`,
+        `FirstName: ${state.name}
+         LastName: ${state.lastName}
+         Age: ${state.age}
+         EmailAddress: ${state.email}
+         Password: ${state.password}`,
       icon: <SmileOutlined style={{ color: '#108ee9' }} />,
     });
   };
@@ -79,7 +78,7 @@ const Register = ( {name = "/register"} ) =>{
         <Button type="primary" htmlType="submit">
           Submit
         </Button>
-        <Button htmlType="button" onClick={goBack}>
+        <Button htmlType="button" onClick={ goBack }>
           Go Back
         </Button>
       </Form.Item>
