787fee8d020ec17a69866a8d30bed42072e0d43a
fernandogutierrez279 <fernandogutierrez279@gmail.com>
2020-10-25 13:33:47 -0400

    Added a new page called Register

---
+++ src/containers/Register/index.js
@@ -0,0 +1,14 @@
+import 'antd/dist/antd.css';
+import React from 'react';
+import { Button, Input } from 'antd';
+
+
+const Register = () =>{
+    return (
+        <div>
+          <h1> Component Register</h1>
+        </div>
+    )
+}
+
+export default Register;
---
+++ src/pages/register.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Register from '../containers/Register';
+import Wraper from '../containers/Wrapper'
+
+
+const RegisterPage = () =>{
+  return (
+     <Wraper>
+        <Register/>
+     </Wraper> 
+  );
+};
+
+export default RegisterPage;
