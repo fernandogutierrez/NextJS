a6e8eac7e0fa03f77d16b7267c2205566222bb1b
fernandogutierrez279 <fernandogutierrez279@gmail.com>
2020-10-25 17:10:57 -0400

    Created and added antd elements to Register container and added css style

--- src/containers/Login/index.js
+++ src/containers/Login/index.js
@@ -1,13 +1,35 @@
-import React from 'react';
+import React, { useEffect,useState } from 'react';
 import { Button, Input } from 'antd';
 import { useRouter } from 'next/router';
 
+const fetch = {
+    status: 200,
+    body: {
+        name: "Daniel",
+        age: 26
+    }
+}
 
-const Login = () =>{
+const Login = ({ name = "Fernando"} ) =>{
     const router = useRouter();
-    console.log(router); 
+    const [state, setState] = useState({ name });
+
+    useEffect(() => {
+        console.log("useEffect")
+        initialState();
+        return () =>    {
+            console.log(`Page: ${router.route} was changed!`);
+        }
+    }, []);
+
+    const initialState = async () =>{
+        setState({ name: fetch.body.name });
+    }
+    console.log(`Page: loaded ${router.route}!`);
+    console.log(state);
+
     const goToRegister = () => {
-        router.push("/register")
+        router.push("/register");
     }
 
     return (
@@ -17,7 +39,7 @@ const Login = () =>{
           <label className="element" >Password </label>
           <Input className="element" placeholder="Enter password" type="password" required/>
           <Button>LOG IN</Button>
-          <Button onClick={goToRegister}>REGISTER</Button>
+          <Button onClick={ goToRegister }>REGISTER</Button>
         <style jsx>
           {`
           #centered{
@@ -27,7 +49,7 @@ const Login = () =>{
             transform: translate(-50%, -50%);
           }
           .container {
-            border-radius: 25px;
+            border-radius: 15px;
             border: 10px solid #034f84;
             padding: 10px;
             width: 400px;
--- src/containers/Register/index.js
+++ src/containers/Register/index.js
@@ -1,14 +1,67 @@
 import 'antd/dist/antd.css';
 import React from 'react';
-import { Button, Input } from 'antd';
-
+import { Form, Input, Button, Checkbox } from 'antd';
 
 const Register = () =>{
-    return (
-        <div>
-          <h1> Component Register</h1>
-        </div>
-    )
+
+  const onFinish = (values) => {
+    console.log('Success:', values);
+  };
+
+  return (
+    <div className="container" id="centered">
+    <Form onFinish={onFinish}>
+      <Form.Item label="FirstName" name="firstName">
+        <Input />
+      </Form.Item>
+
+      <Form.Item label="LastName" name="LastName">
+        <Input />
+      </Form.Item>
+
+      <Form.Item label="Age" name="Age">
+        <Input />
+      </Form.Item>
+
+      <Form.Item label="EmailAddress" name="EmailAddress">
+        <Input />
+      </Form.Item>
+
+      <Form.Item label="Password"name="password">
+        <Input.Password />
+      </Form.Item>
+
+      <Form.Item name="remember" valuePropName="checked">
+        <Checkbox>Remember me</Checkbox>
+      </Form.Item>
+
+      <Form.Item>
+        <Button type="primary" htmlType="submit">
+          Submit
+        </Button>
+      </Form.Item>
+    </Form>
+    <style jsx>
+          {`
+          #centered{
+            position: fixed;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+          }
+          .container {
+            border-radius: 15px;
+            border: 3px solid #034f84;
+            padding: 10px;
+            width: 400px;
+            display: flex;
+            flex-direction: column;
+            align-items: left;
+          }
+        `}
+        </style>
+       </div>
+  );
 }
 
 export default Register;
