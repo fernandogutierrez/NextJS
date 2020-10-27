d960d424caab9f8a5f495da84b4eb2a1812742f8
fernandogutierrez279 <fernandogutierrez279@gmail.com>
2020-10-27 18:07:20 -0400

    [Terms] Added a new image to terms page and fixed content

--- src/containers/Terms/index.js
+++ src/containers/Terms/index.js
@@ -1,25 +1,23 @@
 import React, { useEffect,useState } from 'react';
 
 const Terms = () =>{
-    return ( <div><h1>Terms and Conditions</h1>
-        <h4>
-        
+    return ( <div>
+      
+      <img src="terms/termsandcondition.png" style={{ width: 1480, height:300, marginBottom:20}}/>
+        <p>  Welcome to school page!</p>
         <p>
-           Welcome to school page!
-        These terms and conditions outline the rules and regulations for the use of Atom's Website, located at http://localhost:3000.
+        These terms and conditions outline the rules and regulations for the use of Atom's Website, located at <a href="http://localhost:3000/login"> http://localhost:3000 </a>.
         By accessing this website we assume you accept these terms and conditions. Do not continue to use school page if you do not agree to take all of the terms and conditions stated on this page.
-        
         The following terminology applies to these Terms and Conditions, Privacy Statement and Disclaimer Notice and all Agreements: "Client", "You" and "Your" refers to you, the person log on this website and compliant to the Company’s terms and conditions. "The Company", "Ourselves", "We", "Our" and "Us", refers to our Company. "Party", "Parties", or "Us", refers to both the Client and ourselves. All terms refer to the offer, acceptance and consideration of payment necessary to undertake the process of our assistance to the Client in the most appropriate manner for the express purpose of meeting the Client’s needs in respect of provision of the Company’s stated services, in accordance with and subject to, prevailing law of Netherlands. Any use of the above terminology or other words in the singular, plural, capitalization and/or he/she or they, are taken as interchangeable and therefore as referring to same.
-        <h2>Cookies</h2>
-        
-        We employ the use of cookies. By accessing school page, you agreed to use cookies in agreement with the Atom's Privacy Policy.
-        
-        Most interactive websites use cookies to let us retrieve the user’s details for each visit. Cookies are used by our website to enable the functionality of certain areas to make it easier for people visiting our website. Some of our affiliate/advertising partners may also use cookies.
-        License
-        
-        Unless otherwise stated, Atom and/or its licensors own the intellectual property rights for all material on school page. All intellectual property rights are reserved. You may access this from school page for your own personal use subjected to restrictions set in these terms and conditions.
         </p>
-        </h4> 
+
+        <h1>Cookies</h1>
+        <p>
+          We employ the use of cookies. By accessing school page, you agreed to use cookies in agreement with the Atom's Privacy Policy.
+          Most interactive websites use cookies to let us retrieve the user’s details for each visit. Cookies are used by our website to enable the functionality of certain areas to make it easier for people visiting our website. Some of our affiliate/advertising partners may also use cookies.
+        License
+          Unless otherwise stated, Atom and/or its licensors own the intellectual property rights for all material on school page. All intellectual property rights are reserved. You may access this from school page for your own personal use subjected to restrictions set in these terms and conditions.
+        </p>
          
       </div>)
 }
